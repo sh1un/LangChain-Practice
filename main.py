7e6a8c582ab1e812ea0b1feabde1a04623462fd2
sh1un <a0923183408@gmail.com>
2024-04-03 10:53:15 +0800

    feat(main.py): initial code for chat functionality

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_community.chat_models.bedrock import BedrockChat
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+
+chat = BedrockChat(
+    model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+    model_kwargs={"temperature": 1.0},
+    credentials_profile_name="ecv-bedrock-dev",
+)
+
+messages = [HumanMessage(content="Hello I am shiun")]
+
+response = chat(messages=messages)
+print(response)
