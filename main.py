bf174509fa25916392b5c7e6ebaee3d0eaae2057
sh1un <a0923183408@gmail.com>
2024-04-05 03:47:37 +0800

    feat: add output parser

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureChatOpenAI
 
 load_dotenv()
 
 AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a Cloud Engineer"),
+        ("human", "{input}"),
+    ]
+)
 
 llm = AzureChatOpenAI(
     azure_deployment=AZURE_DEPLOYMENT_NAME,
@@ -13,4 +21,10 @@ llm = AzureChatOpenAI(
     api_version="2023-05-15",
 )
 
-print(llm.invoke(input="Hello I am shiun"))
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+print(
+    chain.invoke(
+        {"input": "How do I monitor EC2 CPU, Your response is limited to 100 words"}
+    )
+)
