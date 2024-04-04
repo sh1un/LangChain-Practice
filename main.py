047e86ff785df183a077e971bbad159a30d0be88
sh1un <a0923183408@gmail.com>
2024-04-04 23:31:50 +0800

    feat: add credentials profile name in BedrockChat initialization

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from langchain_core.messages import HumanMessage
 load_dotenv()
 
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+AWS_CRED_PROFILE_NAME = os.environ.get("AWS_CRED_PROFILE_NAME")
 
 chat = BedrockChat(
     model_id="anthropic.claude-3-sonnet-20240229-v1:0",
     model_kwargs={"temperature": 1.0},
-    credentials_profile_name="ecv-bedrock-dev",
+    credentials_profile_name=AWS_CRED_PROFILE_NAME,
 )
 
 messages = [HumanMessage(content="Hello I am shiun")]
 
-response = chat(messages=messages)
-print(response)
+print(chat(messages=messages))
