45f9b47a03e0f372f8293ec330ef723bf1bc42bd
sh1un <a0923183408@gmail.com>
2024-04-08 15:11:17 +0800

    feat: add search agent by using Tavily API

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 
 from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_functions_agent
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain.tools.retriever import create_retriever_tool
 from langchain_community.document_loaders.web_base import WebBaseLoader
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_core.documents import Document
 from langchain_core.messages import AIMessage, HumanMessage
@@ -47,6 +51,7 @@ llm = AzureChatOpenAI(
     api_version="2023-05-15",
 )
 
+
 output_parser = StrOutputParser()
 
 loader = WebBaseLoader(
@@ -63,8 +68,8 @@ text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents=documents, embedding=embeddings)
 
-document_chain = create_stuff_documents_chain(llm=llm, prompt=prompt_template)
 
+document_chain = create_stuff_documents_chain(llm=llm, prompt=prompt_template)
 
 # print(
 #     document_chain.invoke(
@@ -78,16 +83,42 @@ document_chain = create_stuff_documents_chain(llm=llm, prompt=prompt_template)
 # )
 
 retriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(
-    retriever=retriever, combine_docs_chain=document_chain
+# retrieval_chain = create_retrieval_chain(
+#     retriever=retriever, combine_docs_chain=document_chain
+# )
+
+# Tools
+retriever_tool = create_retriever_tool(
+    retriever=retriever,
+    name="langsmith_search",
+    description="Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
 )
 
+search = TavilySearchResults()
+tools = [retriever_tool, search]
+
+# Agent
+prompt = hub.pull("hwchase17/openai-functions-agent")
+agent = create_openai_functions_agent(llm=llm, tools=tools, prompt=prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 print(
-    retrieval_chain.invoke(
-        {
-            "chat_history": chat_history,
-            "input": "Tell me now",
-        }
+    agent_executor.invoke(
+        {"input": "how can langsmith help with testing?", "chat_history": chat_history}
     )
 )
+print(agent_executor.invoke({"input": "what is the weather in SF?"}))
+
+
+retrieval_chain = create_history_aware_retriever(
+    llm=llm, retriever=retriever, prompt=prompt
+)
+
+# print(
+#     retrieval_chain.invoke(
+#         {
+#             "chat_history": chat_history,
+#             "input": "Tell me now",
+#         }
+#     )
+# )
