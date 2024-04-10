6e2272d92a13789c71ad2bd4002c330cecc77b78
sh1un <a0923183408@gmail.com>
2024-04-10 22:17:06 +0800

    feat: add latest steps

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from langchain_community.callbacks import get_openai_callback
+from langchain_community.callbacks.manager import get_openai_callback
 
 from hugo_agent import build_agent
+
 
 def main():
 
@@ -15,20 +16,37 @@ def main():
     executor_command_prompt = f"""
     {role}
     Now your mission is to solve the problem from customer.
-    Then you have to follow the steps 
-    1. realize the problem
-    2. categorize the problem
-    3. find solutions
-    4. report the problem you have solved
+    Then you have to follow the steps:
+    1. Classify the problem
+
+    2. Compare the classification with the knowledge base
+
+    3. If there is no information in the knowledge base:
+    - Search for information.
+        - Open ticket:
+        1. Bot gives the ticket information to the customer before submitting to the ticket system.
+            - If yes:
+                - Submit the ticket.
+            - If no:
+                - Ask for more information or reconsider the approach.
+
+    4. If there is information in the knowledge base:
+    - Provide solutions.
+        1. If the customer is satisfied with the answer:
+            - Open a ticket.
+        2. If the customer is not satisfied:
+            - Rethink the solution (a limit for rethinking must be set).
+            - If the maximum limit of rethinking is reached:
+                - Escalate to Technical Account Manager (TAM).
 
     Keep noticing how much steps you have completed.
-
     The problem is {scenario}
     """
 
     with get_openai_callback() as cb:
         response = build_agent(executor_command_prompt)
         print(cb)
+
 
 if __name__ == "__main__":
     main()
